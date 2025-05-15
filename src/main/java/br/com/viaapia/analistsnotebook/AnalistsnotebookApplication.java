a8115f2f3905c1e4181b64e2d91da61774bb106b
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-05-15 11:31:43 -0300

    arquivos iniciais enviados para o git

---
+++ src/main/java/br/com/viaapia/analistsnotebook/AnalistsnotebookApplication.java
@@ -0,0 +1,13 @@
+package br.com.viaapia.analistsnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalistsnotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalistsnotebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viaapia/analistsnotebook/AnalistsnotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viaapia.analistsnotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalistsnotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
